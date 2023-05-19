aa49d7f17fedae994c99300c95dc3b2c08ed74d0
Vanshika <srishtidwivedi672@gmail.com>
2023-05-19 18:01:50 +0530

    done

--- server.js
+++ server.js
@@ -6,18 +6,15 @@ const route = require('./src/route/route.js');
 // content-type : application/json
 app.use(express.json());
 
-  
-// Middleware to handle errors (global level middleware)
+app.use('/', route);
+
+// starting server
+app.listen(process.env.PORT, ()=>{
+    console.log(`Express Application is running at port ${process.env.PORT}`);
+})
+
 app.use((err, req, res, next) => {
     // Log the error
     console.error(err);
     return res.status(500).send({status:false, error: 'Internal Server Error', message:err.message, name: err.name});
   });
-
-
-
-app.use('/', route);
-// starting server
-app.listen(process.env.PORT, ()=>{
-    console.log(`Express Application is running at port ${process.env.PORT}`);
-})
